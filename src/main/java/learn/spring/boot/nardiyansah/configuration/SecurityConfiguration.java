3251072a446a5199619b61f5c715e718836687b7
nardiyansah <janatri1398@gmail.com>
2022-02-28 17:04:39 +0700

    for Generating a 401 in the server I still don't try it yet https://spring.io/guides/tutorials/spring-boot-oauth2/

--- src/main/java/learn/spring/boot/nardiyansah/configuration/SecurityConfiguration.java
+++ src/main/java/learn/spring/boot/nardiyansah/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -12,6 +13,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
         http.authorizeRequests(
                 request -> request.antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
@@ -21,6 +24,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/").permitAll()
         ).exceptionHandling(
                 error -> error.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-        ).oauth2Login();
+        ).oauth2Login(
+                httpSecurityOAuth2LoginConfigurer -> httpSecurityOAuth2LoginConfigurer
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+        );
     }
 }
